0e8efa9e4c73a5713799e13f865dc4bc2f448f92
Anshika Bhadoria <anshikabhadoria88@gmail.com>
2024-10-26 10:19:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+
+import Home from './Pages/Home'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ServicePage1 from './Pages/ServicePage1';
+import ContactPage from './Pages/contactPage';
+import AboutPage from './Pages/AboutPage';
+import ServicePage2 from './Pages/ServicePage2';
+import ServicePage3 from './Pages/Servicepage3';
+import ServicePage4 from './Pages/ServicePage4';
+
+
+
+function App() {
+
+  return (
+   <BrowserRouter>
+   <Routes>
+    <Route path= '/' element= {<Home/>}/>
+    <Route path='/Service1'element={<ServicePage1/>}/>
+    <Route path='/Service2'element={<ServicePage2/>}/>
+    <Route path='/Service3'element={<ServicePage3/>}/>
+    <Route path='/Service4'element={<ServicePage4/>}/>
+    <Route path='/contact' element={<ContactPage/>}/>
+    <Route path='/about' element={<AboutPage/>}/>
+
+
+   </Routes>
+   </BrowserRouter>
+  )
+}
+
+export default App
